2efd00bf7c707d4949cc4c6455b0418064a10c88
viniciuskociolek58797 <viniciuskociolek58797@gmail.com>
2024-12-19 16:53:48 +0000

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -7,47 +7,22 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Aplicativo Teste',
+      debugShowCheckedModeBanner: false,  // Remover a tarja de debug
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 53, 226, 139)),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Aplicativo Teste'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -56,70 +31,130 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  bool eSucesso = false; // Variável para controlar o sucesso
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      if (_counter < 100) {
+        _counter++;
+      }
+      if (_counter == 100) {
+        eSucesso = true; // Marca como sucesso quando o contador atinge 100
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+        title: Row(
+          children: [
+            const Icon(Icons.app_registration), // Ícone à esquerda
+            const SizedBox(width: 8), // Espaço entre o ícone e o título
+            Text(widget.title), // Título "Aplicativo Teste"
           ],
         ),
+      ),
+      body: Row(
+        children: [
+          // Coluna esquerda com o quadrado verde e sombra
+          Expanded(
+            flex: 2, // O cubo verde ocupará 2 partes do layout
+            child: Container(
+              height: screenHeight, // Responsivo ao preencher toda a coluna da tela
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 52, 215, 131), // Cor de fundo verde
+                borderRadius: BorderRadius.circular(6), // Bordas arredondadas
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.2),
+                    spreadRadius: 3,
+                    blurRadius: 5,
+                    offset: const Offset(3, 3), // Efeito de sombra
+                  ),
+                ],
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start, // Alinha os ícones à esquerda
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    children: [
+                      Icon(Icons.home, color: Colors.white, size: screenWidth * 0.08),
+                      SizedBox(width: 10),
+                      Text('Home', style: TextStyle(color: Colors.white, fontSize: screenWidth * 0.05)),
+                    ],
+                  ),
+                  SizedBox(height: screenHeight * 0.05),
+                  Row(
+                    children: [
+                      Icon(Icons.search, color: Colors.white, size: screenWidth * 0.08),
+                      SizedBox(width: 10),
+                      Text('Pesquisar', style: TextStyle(color: Colors.white, fontSize: screenWidth * 0.05)),
+                    ],
+                  ),
+                  SizedBox(height: screenHeight * 0.05),
+                  Row(
+                    children: [
+                      Icon(Icons.notifications, color: Colors.white, size: screenWidth * 0.08),
+                      SizedBox(width: 10),
+                      Text('Notificações', style: TextStyle(color: Colors.white, fontSize: screenWidth * 0.05)),
+                    ],
+                  ),
+                  SizedBox(height: screenHeight * 0.05),
+                  Row(
+                    children: [
+                      Icon(Icons.settings, color: Colors.white, size: screenWidth * 0.08),
+                      SizedBox(width: 10),
+                      Text('Ajustes', style: TextStyle(color: Colors.white, fontSize: screenWidth * 0.05)),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 3, // A coluna direita ocupará 3 partes do layout
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Align(
+                alignment: Alignment.centerRight, // Alinha a coluna à direita
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text('Clique no botão para aumentar a taxa de download:'),
+                    Text('$_counter%', style: Theme.of(context).textTheme.headlineMedium),
+                    if (eSucesso) ...[
+                      const SizedBox(height: 20),
+                      const Text(
+                        'Sucesso!',
+                        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.green),
+                      ),
+                      const SizedBox(height: 20),
+                      const Text(
+                        'Toda essa interface faz parte de um projeto teste realizado com Flutter. Projeto realizado por Vinicius O. F. Kociolek para a disciplina de Módulo 11, Desenvolvimento Web do Talento Tech.',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 16, fontWeight: FontWeight.normal, color: Colors.black),
+                      ),
+                    ]
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        child: const Icon(Icons.download_for_offline),
+      ),
     );
   }
 }
